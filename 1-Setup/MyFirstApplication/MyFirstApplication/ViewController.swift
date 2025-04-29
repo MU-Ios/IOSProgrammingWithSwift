0b8d85800519fd79ab6c10c781545a9893e8f317
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-29 23:21:53 +0300

    feat: Connected UI views to code (IBOutlets & IBActions)

--- 1-Setup/MyFirstApplication/MyFirstApplication/ViewController.swift
+++ 1-Setup/MyFirstApplication/MyFirstApplication/ViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblImageText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func btnChangeLblImageText(_ sender: Any) {
+        lblImageText.text = "Data Science"
     }
 }
