d4ab8217238600d0ce719a314ccfeaf328994081
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-30 22:51:24 +0300

    feat: Add and use variables

---
+++ 2-SwiftBasics/1-Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var myName = "Musa"
+var mySurname = "Uyumaz"
+
+print(myName)
+
+myName = "musa"
+
+print(myName)
+
+// Yorum satırı
+
+var userName = "muyumaz"
+
+var internettenGelenEnYeniVeri = 50 //camelCase
+var internetten_gelen_en_yeni_veri = 40 //snake_case
+
