3c408105daf59a195a521eaa5f5c7568fffaacfc
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-30 23:00:51 +0300

    feat: Added examples with different data types

--- 2-SwiftBasics/1-Variables.playground/Contents.swift
+++ 2-SwiftBasics/1-Variables.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//String
 var myName = "Musa"
 var mySurname = "Uyumaz"
 
@@ -13,6 +14,20 @@ print(myName)
 
 var userName = "muyumaz"
 
+userName.uppercased()
+userName.append("1")
+
+//Integer - Int
 var internettenGelenEnYeniVeri = 50 //camelCase
 var internetten_gelen_en_yeni_veri = 40 //snake_case
+
+let myNumber = 50
+myNumber.isMultiple(of: 5)
+
+print(myNumber)
+//myNumber = 30
+
+//Double
+let pi = 3.141519
+
 
