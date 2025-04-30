d9fbf6a9507c019a083428bb88810e442db8637c
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-30 23:18:05 +0300

    feat: Added String to Int and Int to String conversions

--- 2-SwiftBasics/1-Variables.playground/Contents.swift
+++ 2-SwiftBasics/1-Variables.playground/Contents.swift
@@ -16,18 +16,68 @@ var userName = "muyumaz"
 
 userName.uppercased()
 userName.append("1")
+userName.count
 
 //Integer - Int
 var internettenGelenEnYeniVeri = 50 //camelCase
 var internetten_gelen_en_yeni_veri = 40 //snake_case
 
 let myNumber = 50
+//myNumber = 30
 myNumber.isMultiple(of: 5)
 
 print(myNumber)
-//myNumber = 30
+
+var myOtherNumber = 20
+myOtherNumber = 30
+//myOtherNumber = "3"
+//myOtherNumber = 3.14
+
+let myFirstNumber = 80
+let mySecondNumber = 12
+myFirstNumber / mySecondNumber
+
 
 //Double
 let pi = 3.141519
 
+let myFirstDouble = 80.0
+let mySecondDouble = 12.0
 
+myFirstDouble / mySecondDouble
+
+//Boolean
+var myBoolean = true
+myBoolean = false
+
+
+var myString = "Musa"
+
+var myNumber2 : Int32 = 10
+myNumber2 = 50000000
+
+let myNewDouble : Float = 3.14
+
+//defining, tanımlama
+let myString2 : String
+
+//myString2.count
+
+//initialization (değerini atama, başlatma)
+myString2 = "Musa"
+
+var myNewNumber : Int
+
+//myNewNumber = "Mahmut"
+
+myNewNumber = 20
+
+let myStringNumber : String
+myStringNumber = String(myNewNumber)
+
+myStringNumber + "10"
+
+
+let stringNo = "400"
+let integerString = Int(stringNo)
+integerString! + 20
