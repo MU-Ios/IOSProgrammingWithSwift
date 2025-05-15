07573049f52eb466d5d39a6b377646bdf18ceda7
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-15 21:40:20 +0300

    feat: Added while loop examples and usage

---
+++ 2-SwiftBasics/loops.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Loops (Döngüler)
+
+//While Loop
+
+5 == 5
+5 == 4
+5 != 4
+
+3 < 9
+3 < 2
+2 > 1
+2 >= 2
+10 <= 10
+
+var x = 0
+
+x = x + 1
+x += 1
+x = 0
+
+print("Döngü başladı")
+while x < 10 {
+    print(x)
+    x += 1
+}
+print("Döngü Bitti")
