8f4fa4b2f17a4b88a951d76c705aa4ba4fb0de7b
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-15 21:45:15 +0300

    feat: Add for loop examples and variations

--- 2-SwiftBasics/loops.playground/Contents.swift
+++ 2-SwiftBasics/loops.playground/Contents.swift
@@ -28,3 +28,23 @@ while x < 10 {
     x += 1
 }
 print("Döngü Bitti")
+
+// For Loop
+
+var myArray = ["Musa", "Gençay", "Gökhan", "Mehmet", "Zeynep"]
+print(myArray[0])
+
+for name in myArray {
+    print(name.uppercased())
+}
+
+var numberArray = [10,20,30,40,50,60,70,80,90]
+numberArray[0] / 5 * 3
+
+for number in numberArray{
+    print(number / 5 * 3)
+}
+
+for newNumber in 1 ... 10 {
+    print(newNumber)
+}
